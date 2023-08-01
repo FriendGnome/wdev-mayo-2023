b1d0e2ca04e8e1036cb75c8c66d495e3159ddca0
FriendGnome <88346089+FriendGnome@users.noreply.github.com>
2023-07-31 22:15:06 -0400

    .
    .

--- Clases de V/ProyectoJS/js/menu.js
+++ Clases de V/ProyectoJS/js/menu.js
@@ -1,22 +1,23 @@
-//comilla inversa
 menuprincipal.innerHTML += `
-<ul class="nav nav-tabs" id="navId" role="tablist">
-<li class="nav-item">
-    <a href="index.html" class="nav-link active" data-bs-toggle="tab" aria-current="page">Active</a>
-</li>
-<li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-    <div class="dropdown-menu">
-        <a class="dropdown-item" href="">Action</a>
-        <a class="dropdown-item" href="">Another action</a>
-        <div class="dropdown-divider"></div>
-        <a class="dropdown-item" href="#tab4Id">Action</a>
-    </div>
-</li>
-<li class="nav-item" role="presentation">
-    <a href="#tab5Id" class="nav-link" data-bs-toggle="tab">Another link</a>
-</li>
-<li class="nav-item" role="presentation">
-    <a href="#" class="nav-link disabled" data-bs-toggle="tab">Disabled</a>
-</li>
-</ul>`;
+<nav class="navbar navbar-expand-xxl navbar-dark bg-dark">
+<a class="navbar-brand" href="#">Navbar</a>
+<button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+    aria-expanded="false" aria-label="Toggle navigation"></button>
+<div class="collapse navbar-collapse" id="collapsibleNavId">
+    <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+        <li class="nav-item">
+            <a class="nav-link active" href="index.html" aria-current="page">Home <span class="visually-hidden">(current)</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active" href="https://paginas-web-cr.com/ApiPHP" target="_blank" aria-current="page">Api <span class="visually-hidden">(current)</span></a>
+        </li>                
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+            <div class="dropdown-menu" aria-labelledby="dropdownId">
+                <a class="dropdown-item" href="listarcurso.html">Listar Curso</a>
+                <a class="dropdown-item" href="#">Action 2</a>
+            </div>
+        </li>
+    </ul>
+</div>
+</nav>`;
